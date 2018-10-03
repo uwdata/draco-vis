3648e556b5bf45d797f496928c8fe96e74c8283c
Dominik Moritz <domoritz@gmail.com>
2018-10-03 00:50:14 -0700

    v3 schema

--- src/spec.ts
+++ src/spec.ts
@@ -53,7 +53,7 @@ export function asp2vl(facts: string[]): TopLevelFacetedUnitSpec {
   }
 
   return {
-    $schema: 'https://vega.github.io/schema/vega-lite/v2.json',
+    $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
     data: { url: `${url}` },
     mark,
     encoding,
